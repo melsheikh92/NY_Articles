4b0681da513a7e14af05f9152d84125378a9a3c3
Mahmoud <melsheikh92@gmail.com>
2019-03-25 16:05:46 -0700

    Update AppDelegate.swift

--- NY_Articles/App/AppDelegate.swift
+++ NY_Articles/App/AppDelegate.swift
@@ -7,16 +7,18 @@
 //
 
 import UIKit
-
+import moa
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-
+      
+        //init error img for all img loading
+        Moa.errorImage = R.image.calendar()
+        
         return true
     }
 }
